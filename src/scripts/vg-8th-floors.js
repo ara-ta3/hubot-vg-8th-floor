da61f5ad5428cac59a51a8214f0b1740b2e1bc0a
Arata Tanaka <tarata43@yahoo.co.jp>
2016-08-26 21:14:42 +0900

    Impl hubot script

---
+++ src/scripts/vg-8th-floors.js
@@ -0,0 +1,52 @@
+// Description
+//  8F
+//
+// Commands:
+//  hubot パンゲア - display map
+//  hubot メガラニカ - display map
+//  hubot ムー - display map
+//  hubot アトランティス - display map
+//  hubot パシフィック - display map
+//  hubot ジパング - display map
+//  hubot レムリア - display map
+//  hubot アフリカ - display map
+//  hubot ユーラシア - display map
+//  hubot ノースアメリカ - display map
+//  hubot サウスアメリカ - display map
+//  hubot オーストラリア - display map
+//  hubot アンタークティカ - display map
+//  hubot アジト - display map
+
+const vg8thFloor = require("node-vg-8th-floors");
+const stringMap = {
+    "パンゲア": "pangea",
+    "メガラニカ": "megallanica",
+    "ムー": "mu",
+    "アトランティス": "atlantis",
+    "パシフィス": "pacifis",
+    "ジパング": "zipang",
+    "レムリア": "lemuria",
+    "アフリカ": "africa",
+    "ユーラシア": "eurasia",
+    "ノースアメリカ": "northAmerica",
+    "サウスアメリカ": "southAmerica",
+    "オーストラリア": "australlia",
+    "アンタークティカ": "antarctica",
+    "アジト": "ajito"
+};
+
+module.exports = (robot) => {
+    robot.respond(/(.+)$/i, function(msg) {
+        const matched = msg.match[1];
+        try {
+            const floor = vg8thFloor(stringMap[matched]);
+            msg.send([
+                "```",
+                floor,
+                "```"
+            ].join("\n"));
+        } catch (e) {
+            robot.logger.error(e);
+        }
+    });
+};
